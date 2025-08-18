740b7c52b6bf4ca55498faad9f98411d46617f43
Tiago Rinaldi <trinaldi@gmail.com>
2025-08-18 08:23:52 -0300

    Project structure (#2)
    * chore: update gitignore
    
    * chore: remove initial project assests
    
    * feat: add project structure for now
    
    you can call this a v1 structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "../pages/LoginPage";
-import Dashboard from "../pages/Dashboard";
-import './App.css';
+import Login from "../src/pages/Login";
+import Dashboard from "../src/pages/Dashboard";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
