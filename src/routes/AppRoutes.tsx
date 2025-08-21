167c86acd4eefbe413d36f4b1eb4ece6150013e0
Tiago Rinaldi <trinaldi@gmail.com>
2025-08-21 09:14:27 -0300

    feat: add sidebar to project (#5)

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import DashboardPage from "../pages/DashboardPage";
 import { useAuth } from "../context/AuthContext";
+import Layout from "@/components/common/Layout";
 import React from "react";
 
 function PrivateRoute({ children }: { children: React.ReactNode }) {
@@ -13,7 +14,16 @@ export function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
+            <Route
+                path="/dashboard"
+                element={
+                    <PrivateRoute>
+                        <Layout>
+                            <DashboardPage />
+                        </Layout>
+                    </PrivateRoute>
+                }
+            />
         </Routes>
     );
 }
