346b3d7c819a483a860e0d17c65186ab27f4dfc8
Tiago Rinaldi <trinaldi@gmail.com>
2025-08-24 09:25:02 -0300

    Userform (#9)
    * fix: fix addresstype
    
    * feat: add user profile type/component

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,7 +1,9 @@
 import DonationCardList from "../components/Donation/DonationCardList.tsx";
 import { Box, Container } from "@mui/material";
 import { useDonations } from "@/hooks/useDonations.tsx";
-import Address from "@/components/Address/Address.tsx";
+import type { Address as AddressType } from "@/types/address.ts";
+import type { UserProfileType } from "@/types/user.ts";
+import UserProfile from "@/components/User/UserProfile.tsx";
 
 const fakeAddress: AddressType = {
   street: "rua dos bobos, 0",
@@ -11,11 +13,23 @@ const fakeAddress: AddressType = {
   zipCode: "14026590",
 };
 
-function DashboardPage() {
-  const { donations, loading, error} = useDonations();
 
-  if (loading) return <p>loading...</p>
-  if (error) return <p>{error}</p>
+function DashboardPage() {
+  const { donations, loading, error } = useDonations();
+
+  const fakeUser: UserProfileType = {
+    "userId": 1,
+    "avatarUrl": "https://thumbs.dreamstime.com/b/unknown-male-avatar-profile-image-businessman-vector-unknown-male-avatar-profile-image-businessman-vector-profile-179373829.jpg",
+    "name": "fulano de tal",
+    "phone": "11988854567",
+    "cpf": "12345678909",
+    "email": "fulano@email.com",
+    "address": fakeAddress,
+    "donations": donations ?? []
+  }
+
+  if (loading) return <p>loading...</p>;
+  if (error) return <p>{error}</p>;
   return (
     <Box
       sx={{
@@ -26,8 +40,7 @@ function DashboardPage() {
       }}
     >
       <Container maxWidth="lg">
-        <DonationCardList donations={donations}/>
-        <Address address={fakeAddress} category="delivery" />
+        <UserProfile user={fakeUser} />
       </Container>
     </Box>
   );
